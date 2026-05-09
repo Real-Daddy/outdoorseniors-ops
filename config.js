cb34391c483f2805c2cde76815a9056a0796bd5c
Real-Daddy <ballipalliravindranath@gmail.com>
2026-05-09 17:50:19 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Firebase config
 const firebaseConfig = {
-  apiKey: "AIzaSyC2tSrnPKhSl6eGUiEwD8eUCR0a-yqVHVQ",
-  authDomain: "outdoorseniors-6375a.firebaseapp.com",
-  projectId: "outdoorseniors-6375a",
-  storageBucket: "outdoorseniors-6375a.firebasestorage.app",
+  apiKey:            "AIzaSyC2tSrnPKhSl6eGUiEwD8eUCR0a-yqVHVQ",
+  authDomain:        "outdoorseniors-6375a.firebaseapp.com",
+  projectId:         "outdoorseniors-6375a",
+  storageBucket:     "outdoorseniors-6375a.firebasestorage.app",
   messagingSenderId: "860677619929",
-  appId: "1:860677619929:web:27ad3518799df02c49bec8",
-  measurementId: "G-ECRP1QX03V"
+  appId:             "1:860677619929:web:27ad3518799df02c49bec8",
+  measurementId:     "G-ECRP1QX03V"
 };
+
+// Gemini API key
+const GEMINI_API_KEY = "AIzaSyBatwMJkioSfCrKIpoeoKbLaRph6vRUFBE";
